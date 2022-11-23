fd258279821173d0f6f0cb85f9aae7a259540d5a
tomoaki_narukawa <tomo111911111@gmail.com>
2022-11-23 15:27:52 +0900

    sessionsコントローラーを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  skip_before_action :login_required, only: [:new, :create]
+
   def new
     @user = User.new
   end
@@ -7,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to_user_path(@user.id)
+      redirect_to user_path(@user.id)
     else
       render :new
     end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
 end
