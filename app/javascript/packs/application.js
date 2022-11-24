fbed0ec7db5b2edbca7c48e558e3612cd88952cb
tomoaki_narukawa <tomo111911111@gmail.com>
2022-11-24 23:24:30 +0900

    users/new、session/new、ヘッダーのcss実装完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import 'bootstrap';
 import '../stylesheets/application';
+import '../stylesheets/base';
 
 Rails.start();
 Turbolinks.start();
