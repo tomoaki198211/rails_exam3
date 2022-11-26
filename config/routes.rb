ca176ce001d7bc80148e2b8efcb24886562b8c8f
tomoaki_narukawa <tomo111911111@gmail.com>
2022-11-26 20:52:33 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'posts#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update] do
     get :favorites, on: :collection
